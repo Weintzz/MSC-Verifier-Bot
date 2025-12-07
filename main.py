d9af316e4fa0d87b37f7635e21d82adc1b3be819
Weintzz <ry3ngt@gmail.com>
2025-12-07 14:48:43 +0800

    Added verify slash command which takes in msc id, sn, and email of user

--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
+from discord import app_commands
 import os
 
 load_dotenv()
+GUILD_ID = discord.Object(id=int(os.getenv('GUILD_ID')))
 
-class Client(discord.Client):
+
+class Client(commands.Bot):
     async def on_ready(self):
         print(f'Logged in as {self.user}!')
+        
+        try:
+            synced = await self.tree.sync(guild=GUILD_ID)
+            print(f'Synced {len(synced)} command(s)')
+        except Exception as e:
+            print(e)
 
     async def on_message(self, message):
+        if message.author == self.user:
+            return
+        
         print(f'{message.author} : {message.content}')
+        
+        if message.content == "hello":
+            await message.channel.send(f"hi")
 
 
 intents = discord.Intents.default()
 intents.message_content = True
+client = Client(command_prefix="!", intents=discord.Intents.all())
 
-client = Client(intents=intents)
+@client.tree.command(name="verify", description="verifies your membership", guild=GUILD_ID)
+async def verifyMember(interaction: discord.Interaction, msc_id: str, student_number: str, email: str):
+    await interaction.response.send_message(f"Confirm: \nMSC ID: {msc_id}\nStudent Number: {student_number}\nEmail: {email}",
+    ephemeral=True)
+
+
 client.run(os.getenv('BOT_TOKEN'))

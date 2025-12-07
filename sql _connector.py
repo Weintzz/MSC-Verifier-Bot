4a02562f097f4a5b09851dbf53186365d6c524d4
James <jameslaurencebelen123@gmail.com>
2025-12-07 14:11:27 +0800

    added a psuedo verification system

---
+++ sql _connector.py
@@ -0,0 +1,42 @@
+import mysql.connector
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+def initialize():
+    try:
+        conn = mysql.connector.connect(
+            host=os.getenv("host"),       # e.g., mysql.hostinger.com
+            user=os.getenv("user"),   # your MySQL username
+            password=os.getenv("password"),
+            database=os.getenv("database")
+        )
+
+        print("Connected to database!")
+        return conn
+
+    except mysql.connector.Error as err:
+        print("Error:", err)
+        return None
+
+def close(conn):
+    conn.close()
+
+
+#things to get in order to verify: MSC ID, BULSU ID, email
+def verify(conn, msc_id, student_no, email):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM students WHERE msc_id=%s AND student_no=%s AND email=%s", (msc_id, student_no, email))
+    result = cursor.fetchone() # nakatuple or list pala ung result neto kaya by index mo iaaccess
+
+    if result:
+        print(f"You are now verified, {result[9]}")
+        #dito ilalagay ung sa discord side  
+    else:
+        print("Verificaition failed. Wrong MSC ID/Student ID/Personal Email")
+
+conn = initialize()
+verify(conn, "MSCEB252619", "2023104357", "jameslaurencebelen@gmail.com")
+close(conn)

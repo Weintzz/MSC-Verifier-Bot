4ef22ebda58a1570f07495ca914b66df89894e49
Weintzz <ry3ngt@gmail.com>
2025-12-07 13:21:41 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import discord
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+class Client(discord.Client):
+    async def on_ready(self):
+        print(f'Logged in as {self.user}!')
+
+    async def on_message(self, message):
+        print(f'{message.author} : {message.content}')
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = Client(intents=intents)
+client.run(os.getenv('BOT_TOKEN'))
